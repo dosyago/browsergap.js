406658ef167f5e3bcc38e496621720e7f46bf624
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-08-08 11:20:45 +0000

    "OK"

--- browsergap.js
+++ browsergap.js
@@ -1,10 +1,29 @@
+const SERVICE = action => `https://api.browsergap.dosyago.com/v1/${action}`;
+
 class BrowserGapAPI {
   constructor(apiKey) {
     this.apiKey = apiKey;
   }
 
   openSecurely(url) {
-    
+    const {apiKey} = this;
+
+    const portal = window.open('about:blank');
+
+    const {browserUrl, err} = await fetch(SERVICE('new.browser'), {
+      method: 'POST',
+      content-type: 'application/json',
+      body: {
+        url, 
+        apiKey
+      }
+    }).then(r => r.json());
+
+    if ( err ) {
+      throw err;
+    } else {
+      portal.location = browserUrl;
+    }
   }
 };
 
